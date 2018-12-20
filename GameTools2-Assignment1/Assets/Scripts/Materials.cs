ff155fd8661190ef7f0abe5a5b67edb7b5ae5e3d
Jason McEvoy <C17317363@mydit.ie>
2018-12-20 03:27:57 +0000

    texture fix

---
+++ GameTools2-Assignment1/Assets/Scripts/Materials.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Materials : MonoBehaviour {
+    public Material matofoject;
+    public Color newcol;
+    public KeyCode changecolo;
+	// Use this for initialization
+	void Start () {
+        matofoject.color = Color.black;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(changecolo))
+        {
+            if (matofoject.color == Color.black){
+                matofoject.color = newcol;
+            }
+            else
+            {
+                matofoject.color = Color.black;
+            }
+        }
+	}
+}
